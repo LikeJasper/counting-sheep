399f8161e9ed8ca15e0c8b10c6936bb13bd1865f
LikeJasper <williamclark.home@gmail.com>
2015-11-04 16:38:35 +0100

    Sheep appear large but offset then shrink into place

--- more-sheep.js
+++ more-sheep.js
@@ -1,11 +1,16 @@
-var anotherSheepPlease;
-
 $(function () {
   'use strict';
-  anotherSheepPlease = function () {
-    var elem = '<img src="sheep.gif" alt="sheep">';
-    $('.container').append(elem);
-    console.log("Here you go");
+  var i = 1;
+  var anotherSheepPlease = function () {
+    var sheeptml = '<img src="img/sheep.gif" alt="sheep' + i + '" class="new">',
+      sheepDimensions = [260, 179],
+      $elem = $(sheeptml).appendTo('.container');
+
+    $elem
+      .width('10vw')
+      .css('top', (Math.floor((i-1) / 10) * (sheepDimensions[1] / sheepDimensions[0]) * 10).toString() + 'vw')
+      .css('left', (((i-1) % 10) * 10).toString() + 'vw');
+    i++;
     setTimeout(anotherSheepPlease, 1000);
   };
   anotherSheepPlease();
