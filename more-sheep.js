2eee7b720a77c44abae59a98ccc9c42691557419
LikeJasper <williamclark.home@gmail.com>
2015-11-04 16:58:47 +0100

    Cleaning up and made small sheep smaller

--- more-sheep.js
+++ more-sheep.js
@@ -4,12 +4,13 @@ $(function () {
   var anotherSheepPlease = function () {
     var sheeptml = '<img src="img/sheep.gif" alt="sheep' + i + '" class="new">',
       sheepDimensions = [260, 179],
+      smallWidth = 5,
       $elem = $(sheeptml).appendTo('.container');
 
     $elem
-      .width('10vw')
-      .css('top', (Math.floor((i-1) / 10) * (sheepDimensions[1] / sheepDimensions[0]) * 10).toString() + 'vw')
-      .css('left', (((i-1) % 10) * 10).toString() + 'vw');
+      .width(smallWidth.toString() + 'vw')
+      .css('top', (Math.floor((i-1) / (100/smallWidth)) * (sheepDimensions[1] / sheepDimensions[0]) * smallWidth).toString() + 'vw')
+      .css('left', (((i-1) % (100/smallWidth)) * smallWidth).toString() + 'vw');
     i++;
     setTimeout(anotherSheepPlease, 1000);
   };
